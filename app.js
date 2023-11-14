775e2afeec49d0f3c65f2c29395564b26f301610
Kenneth Tran <kenneth.tran@jbhunt.com>
2023-11-14 12:44:34 -0600

    added logic to generate buttons, checkbingo, endgame

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const cells = document.querySelectorAll("td");
 const usedNumbers = new Set();
 const usedAnswers = new Set();
-const answerSets = new Set();
 
 const questionText = document.getElementById("question");
-const answerButton = document.querySelector(".button-container");
-// Container to hold the buttons
-const buttonContainer = document.getElementById("buttonContainer");
 
 const bingoSection = document.querySelector(".bingo");
 const newgameBtn = document.getElementById("newgame");
 const submitBtn = document.getElementById("submit");
+const playBtn = document.getElementById("play");
+const gameScreen = document.querySelector(".game-screen");
+const loadingScreen = document.querySelector(".loading");
+const winMessage = document.getElementById("win-message");
+const loseMessage = document.getElementById("lose-message");
+const firework = document.getElementsByClassName("firework");
 
+const buttons = document.querySelectorAll(".guess-btn");
 let num1, num2, answer; //set this variables to be global
 let markedCells = new Set();
 let gameEnded = false;
@@ -30,6 +33,20 @@ const bingoRows = [
   [2, 4, 6],
 ];
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 // Function to generate and set random numbers from 1 to 20 in each cell
 function generateRandomNumbers() {
   for (let i = 0; i < cells.length; i++) {
@@ -41,19 +58,6 @@ function generateRandomNumbers() {
     cells[i].textContent = randomNum;
   }
 }
-console.log(usedNumbers);
-// Function to generate a random addition question
-// function generateQuestion() {
-//   // Pick a random number from usedNumbers Set
-//   const targetNumber = [...usedNumbers][
-//     Math.floor(Math.random() * usedNumbers.size)
-//   ];
-//   // Find two numbers whose sum equals the targetNumber
-//   // let num1, num2; //these variables need to be global so submitAnswer() can access and check the answer
-//   num1 = Math.floor(Math.random() * targetNumber);
-//   num2 = targetNumber - num1;
-//   return [num1, num2];
-// }
 
 // Function to generate a random unique answer
 function generateUniqueAnswer() {
@@ -62,7 +66,23 @@ function generateUniqueAnswer() {
   } while (usedAnswers.has(answer));
 
   usedAnswers.add(answer);
+  console.log(answer, "answer");
   return answer;
+}
+
+function generateButtonsHTML() {
+  const answersSet = [
+    answer,
+    getRandomInt(1, 20),
+    getRandomInt(0, 20),
+    getRandomInt(0, 20),
+  ];
+
+  shuffleArray(answersSet);
+
+  for (let i = 0; i < answersSet.length; i++) {
+    buttons[i].innerHTML = answersSet[i];
+  }
 }
 
 // Function to generate and display the next question
@@ -73,71 +93,16 @@ function generateNextQuestion(targetNumber) {
 
   // Construct the addition expression
   const additionExpression = `${num1} + ${num2}`;
-  questionText.textContent = "What is " + additionExpression + " = ?";
+  questionText.textContent = "What is " + additionExpression + "?";
 
   // Return an object with the expression and the correct answer
   return additionExpression;
 }
 
-function generateAnswerButtonsHTML(randomNumber) {
-  // Create the button element
-  const buttonElement = document.createElement("button");
-  buttonElement.className = "button item";
-  buttonElement.id = "answer";
-  buttonElement.type = "button";
-
-  // buttonElement.appendChild(spanElement);
-
-  // Append the button element to the document
-  document.getElementById("buttonContainer").appendChild(buttonElement);
-  buttonElement.textContent = `${randomNumber}`;
-
-  // Append the span element as a child of the button element
-  // buttonElement.appendChild(spanElement);
-
-  // Append the button element to the container
-  buttonContainer.appendChild(buttonElement);
-}
-
-// Generate 3 randoms answers
-function generateAnswerButtons() {
-  const randomNumbers = [];
-  answerButton.innerHTML = ""; // Clear previous content
-  console.log(randomNumbers, "should be a empty array random answers");
-
-  for (let i = 0; i < 4; i++) {
-    const randomNumber = Math.floor(Math.random() * 20) + 1;
-    randomNumbers.push(randomNumber);
+function clearButtonText() {
+  for (let i = 0; i < 3; i++) {
+    buttons[i].innerHTML = "";
   }
-
-  let randomIndex = Math.floor(Math.random() * randomNumbers.length);
-  let removedNumber = randomNumbers.splice(randomIndex, 1);
-  // Insert the new number at the random index
-  randomNumbers.splice(randomIndex, 0, answer);
-
-  // Check for duplicates and replace them with unique numbers
-  for (let i = 0; i < randomNumbers.length; i++) {
-    for (let j = i + 1; j < randomNumbers.length; j++) {
-      if (randomNumbers[i] === randomNumbers[j]) {
-        // Replace duplicate with a new unique number
-        randomNumbers[j] = generateUniqueRandomNumber(randomNumbers);
-      }
-    }
-  }
-
-  for (let i = 0; i < 4; i++) {
-    generateAnswerButtonsHTML(randomNumbers[i]);
-  }
-  return randomNumbers;
-}
-
-// Function to generate a unique random number
-function generateUniqueRandomNumber(existingNumbers) {
-  let newNumber;
-  do {
-    newNumber = Math.floor(Math.random() * 10) + 1; // You can adjust the range as needed
-  } while (existingNumbers.includes(newNumber));
-  return newNumber;
 }
 
 // Function to handle answer submission
@@ -145,29 +110,77 @@ function generateUniqueRandomNumber(existingNumbers) {
 // change the question field to correct/wrong according to the answer
 // then set to next question
 
-function submitAnswer() {
-  const playerAnswer = parseInt(answerInput.value);
+function submitAnswer(value) {
+  const playerAnswer = value;
 
-  if (!isNaN(playerAnswer)) {
-    if (playerAnswer === answer) {
-      // Correct answer
-      cells.forEach((cell, index) => {
-        if (cell.textContent == answer && !cell.classList.contains("marked")) {
-          cell.classList.add("marked");
-          markedCells.add(index);
-        }
-      });
-
-      // usedAnswers.delete(currentAnswer); // Remove the answer from usedAnswers
-      generateNextQuestion(generateUniqueAnswer());
-      answerInput.value = ""; //reset input field
-      console.log(usedAnswers);
+  if (playerAnswer == answer) {
+    // Correct answer
+    cells.forEach((cell, index) => {
+      if (cell.textContent == answer && !cell.classList.contains("marked")) {
+        cell.classList.add("marked");
+        markedCells.add(index);
+      }
+    });
+    if (checkForBingo()) {
+      console.log("WIN");
+      endGame("win");
     } else {
-      // Wrong answer
-      questionText.textContent = "Wrong answer!";
-      answerInput.value = "";
-      // endGame();
+      setTimeout(() => {
+        clearButtonText();
+        generateNextQuestion(generateUniqueAnswer());
+        generateButtonsHTML();
+      }, 500);
     }
+
+    // usedAnswers.delete(currentAnswer); // Remove the answer from usedAnswers
+  } else {
+    // Wrong answer
+    questionText.textContent = "Wrong answer!";
+    endGame("lose");
+  }
+}
+
+function checkForBingo() {
+  // Check rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      cells[i * 3].classList.contains("marked") &&
+      cells[i * 3 + 1].classList.contains("marked") &&
+      cells[i * 3 + 2].classList.contains("marked")
+    ) {
+      return true;
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      cells[i].classList.contains("marked") &&
+      cells[i + 3].classList.contains("marked") &&
+      cells[i + 6].classList.contains("marked")
+    ) {
+      return true;
+    }
+  }
+
+  // Check diagonals
+  if (
+    cells[0].classList.contains("marked") &&
+    cells[0].classList.contains("marked") ===
+      cells[4].classList.contains("marked") &&
+    cells[0].classList.contains("marked") ===
+      cells[8].classList.contains("marked")
+  ) {
+    return true;
+  }
+  if (
+    cells[2].classList.contains("marked") &&
+    cells[2].classList.contains("marked") ===
+      cells[4].classList.contains("marked") &&
+    cells[2].classList.contains("marked") ===
+      cells[6].classList.contains("marked")
+  ) {
+    return true;
   }
 }
 
@@ -176,21 +189,57 @@ function newGame() {
   handleHidden();
   usedNumbers.clear();
   generateRandomNumbers();
-  generateNextQuestion();
+  generateNextQuestion(generateUniqueAnswer());
+  generateButtonsHTML();
+}
+
+function endGame(condition) {
+  gameEnded = true;
+  if (condition == "win") {
+    winMessage.style.display = "block";
+    for (let i = 0; i < 3; i++) {
+      firework[i].style.display = "flex";
+    }
+  } else {
+    loseMessage.style.display = "block";
+  }
 }
 
 function handleHidden() {
-  bingoSection.classList.add("hidden");
+  //   bingoSection.classList.add("hidden");
+  winMessage.style.display = "none";
+  loseMessage.style.display = "none";
+  cells.forEach((cell, index) => {
+    if (cell.classList.contains("marked")) {
+      cell.classList.remove("marked");
+    }
+  });
+  for (let i = 0; i < 3; i++) {
+    firework[i].style.display = "none";
+  }
 }
 
 function displayBingo() {
   bingoSection.classList.remove("hidden");
 }
 
+function loadGameScreen() {
+  loadingScreen.classList.add("hidden");
+  gameScreen.classList.remove("hidden");
+}
+
+playBtn.addEventListener("click", loadGameScreen);
+
 // newgameBtn.addEventListener("click", newGame);
 // submitBtn.addEventListener("click", displayBingo);
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    submitAnswer(button.innerHTML);
+  });
+});
 
 // Initialize the game
 generateRandomNumbers();
 generateNextQuestion(generateUniqueAnswer());
-generateAnswerButtons();
+generateButtonsHTML();
